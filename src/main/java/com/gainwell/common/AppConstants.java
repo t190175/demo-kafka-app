0a08a7da140bd6d02b19c24cee4dcb6595cc3d73
akandakatla <akandakatla@host.docker.internal>
2022-03-22 12:04:01 -0500

    Change the Topic Name

--- src/main/java/com/gainwell/common/AppConstants.java
+++ src/main/java/com/gainwell/common/AppConstants.java
@@ -2,7 +2,7 @@ package com.gainwell.common;
 
 public class AppConstants {
 
-	public static final String TOPIC_NAME = "sample-topic";
+	public static final String TOPIC_NAME = "CreateProvider";
 	public static final String GROUP_ID = "group-id";
 
 }
