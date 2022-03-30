344c330ec6ae3eefe803632065f9eddf00686af0
Kandakatla <anil.kandakatla@gainwelltechnologies.com>
2022-03-30 11:47:14 -0500

    change topic CreateProvider

--- src/main/java/com/gainwell/common/AppConstants.java
+++ src/main/java/com/gainwell/common/AppConstants.java
@@ -2,7 +2,7 @@ package com.gainwell.common;
 
 public class AppConstants {
 
-	public static final String TOPIC_NAME = "Newtopic";
+	public static final String TOPIC_NAME = "CreateProvider";
 	public static final String GROUP_ID = "Group-id";
 
 }
